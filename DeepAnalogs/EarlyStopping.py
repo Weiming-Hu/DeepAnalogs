01789fc2a9cae1647534f74526b14a6579ad2bbd
Weiming Hu <weiming@comet-ln3.sdsc.edu>
2022-02-22 08:24:22 -0800

    add early stopping file

---
+++ DeepAnalogs/EarlyStopping.py
@@ -0,0 +1,22 @@
+#############################################################
+# Author: Weiming Hu <weiminghu@ucsd.edu>                   #
+#                                                           #
+#         Center for Western Weather and Water Extremes     #
+#         Scripps Institution of Oceanography               #
+#         UC San Diego                                      #
+#                                                           #
+#         https://weiming-hu.github.io/                     #
+#         https://cw3e.ucsd.edu/                            #
+#                                                           #
+# Date of Creation: 2022/02/22                              #
+#############################################################
+#
+# This script implements the early stopping criteria.
+#
+
+import torch.nn as nn
+
+
+class EarlyStopping:
+
+
