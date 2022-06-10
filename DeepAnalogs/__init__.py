e42bc040af34aa1eecdf52684fd885ba5150d153
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-11 01:24:50 +0800

    Delete DeepAnalogs directory

--- DeepAnalogs/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DeepAnalogs: Train a deep network for weather analog identification!
-"""
-
-__title__ = "DeepAnalogs"
-__author__ = "Weiming Hu"
-
-from DeepAnalogs.version import __version__
-
